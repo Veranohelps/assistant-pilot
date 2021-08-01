7cd8bbff6d505b297f1caf0423d5cb958872030d
Juan Delgado <juan@ustwo.com>
2021-08-01 22:17:59 +0200

    Improves the extraction of coordinates from the GeoJSON route files

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -16,6 +16,20 @@ interface Expedition {
   routes: Array<Route>;
 }
 
+interface Location {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+}
+
+interface Geometry {
+  coordinates: Array<Array<number>>;
+}
+
+interface Feature {
+  geometry: Geometry;
+}
+
 app.use(helmet());
 app.use(cors());
 app.get("/", (req, res) => {
@@ -41,16 +55,30 @@ app.get("/routes/:id", (req, res) => {
   const geoJson = JSON.parse(
     fs.readFileSync(`./static/${req.params.id}.geojson`, "utf-8")
   );
-  const jsonCoordinates = geoJson.features[0].geometry.coordinates as Array<
-    Array<number>
-  >;
 
-  // NOTE (JD): WATCH OUT, geoJson file returns longitude first!!
-  const coordinates = jsonCoordinates.map((coordinate) => ({
-    latitude: coordinate[1],
-    longitude: coordinate[0],
-    altitude: coordinate[2],
-  }));
+  let coordinates: Array<Location> = [];
+
+  // eslint-disable-next-line array-callback-return
+  geoJson.features.map((feature: Feature) => {
+    const jsonCoordinates = feature.geometry.coordinates as Array<
+      Array<number>
+    >;
+    coordinates = coordinates.concat(
+      jsonCoordinates.map(
+        (coordinate) =>
+          ({
+            latitude: coordinate[1],
+            longitude: coordinate[0],
+            altitude: coordinate[2],
+          } as Location)
+      )
+    );
+  });
+
+  coordinates = coordinates.filter(
+    (location: Location) =>
+      location.altitude != null && location.longitude != null
+  );
 
   res.json({
     name: geoJson.features[0].properties.name,
