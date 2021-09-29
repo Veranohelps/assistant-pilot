3827f9aa76fce788eb892c01db95318ba6029f91
Juan Delgado <jd@juandelgado.es>
2021-09-29 17:20:57 +0200

    Temporary enable Strapi auto migrations

--- services/strapi/config/database.js
+++ services/strapi/config/database.js
@@ -13,7 +13,7 @@ module.exports = ({ env }) => ({
         ssl: env.bool("DATABASE_SSL", false),
       },
       options: {
-        autoMigration: false
+        autoMigration: true,
       },
     },
   },
