1d15c6da1ff39d3023e56d9663c35a13844b3947
Kossy Ugochukwu <kossyugochukwu@gmail.com>
2021-12-09 10:50:13 +0100

    format date

--- services/api2/src/modules/common/utilities/success-response.ts
+++ services/api2/src/modules/common/utilities/success-response.ts
@@ -1,11 +1,42 @@
+import _ from 'lodash';
+
 export class SuccessResponse {
   constructor(private message: string, private data?: Record<string, unknown>) {}
+
+  formatDate(date: Date) {
+    const tzo = -date.getTimezoneOffset(),
+      dif = tzo >= 0 ? '+' : '-',
+      pad = (num: number) => {
+        const norm = Math.floor(Math.abs(num));
+        return (norm < 10 ? '0' : '') + norm;
+      };
+
+    const dateString = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(
+      date.getDate(),
+    )}T${pad(date.getHours())}:${pad(date.getMinutes())}${dif}${pad(tzo / 60)}:${pad(tzo % 60)}`;
+
+    return dateString;
+  }
+
+  transform(data: Record<string, unknown>) {
+    const transformed = _.cloneDeepWith(data, (value, key) => {
+      // do not clone coordinate data
+      if (key === 'coordinate') {
+        return value;
+      }
+      if (value instanceof Date) {
+        return this.formatDate(value);
+      }
+    });
+
+    return transformed;
+  }
 
   toJSON() {
     const response = {
       success: true,
       message: this.message,
-      data: this.data,
+      data: this.data ? this.transform(this.data) : undefined,
     };
 
     return response;
