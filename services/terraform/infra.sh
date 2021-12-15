ca2f06d0dbb35888a33d2131b4edc707f5db62ab
Juan Delgado <jd@juandelgado.es>
2021-12-15 12:46:58 +0100

    Upgrades Terraforn to 1.1.0

--- services/terraform/infra.sh
+++ services/terraform/infra.sh
@@ -4,7 +4,7 @@ docker run --rm -it \
 	-w /home/terraform \
 	-v $GOOGLE_APPLICATION_CREDENTIALS:/home/sa.json \
 	-e GOOGLE_APPLICATION_CREDENTIALS=/home/sa.json \
-	hashicorp/terraform:1.0.3 \
-	apply \
-	-var="dersu_api_docker_image_tag=juan-6d3114a" \
-	-var="dersu_admin_console_docker_image_tag=juan-6d3114a"
+	hashicorp/terraform:1.1.0 \
+	plan \
+	-var="dersu_api_docker_image_tag=develop-421001c" \
+	-var="dersu_admin_console_docker_image_tag=develop-421001c"
